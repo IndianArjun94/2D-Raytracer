aadf5264ca3b9dbb37ceb12db993a399a0c0b147
angam <arjun459459459@gmail.com>
2025-10-26 16:49:38 -0500

    Gradle for PC

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,10 +7,14 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven("https://jcuda.org/maven")
 }
 
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    implementation("org.jcuda:jcuda:11.8.0")
+    implementation("org.jcuda:jcuda-natives:11.8.0:windows-x86_64")
 }
